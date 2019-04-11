f6fe55fe11ec0a8e169da1f56f9678e09e1f9e85
ryantxu <ryanxu@wustl.edu>
2019-04-10 20:24:02 -0500

    adjustments

--- structfinder.py
+++ structfinder.py
@@ -2,17 +2,20 @@ import sys, os, re
 
 def structfind(struct, sourcecode, headerfolder):
     sourcecode = open(sourcecode, "r+")
-    newfile = open("cache/structure_info.txt", "w")
+
     found = False
 
     parser(struct, sourcecode)
 
-    for filename in os.listdir(headerfolder):
-        while(found == False):
-            headerfile = open(filename, "r+")
-            parser(struct, headerfile)
+    if(headerfolder != ""):
+        for filename in os.listdir(headerfolder):
+            while(found == False):
+                headerfile = open(filename, "r+")
+                parser(struct, headerfile)
 
 def parser(struct, file):
+    newfile = open("cache/structure_info.txt", "w")
+
     target = "struct " + struct
     line = file.readline()
     while target not in line:
@@ -46,4 +49,4 @@ def parser(struct, file):
     newfile.close()
 
 
-structfind('apev2_hdr_ftr', 'test/structtest.c', 'a')
+structfind('apev2_hdr_ftr', 'test/structtest.c', '')
